843afbe48488297fe210a68ae26f0167e820bf2b
Stanislav Datskovskiy <stas@loper-os.org>
2012-02-16 16:48:55 -0500

    ml501 stuff

---
+++ ml501/hpi_controller.v
@@ -0,0 +1,159 @@
+/*************************************************************************
+ *                (c) Copyright 2012 Stanislav Datskovskiy                *
+ *                         http://www.loper-os.org                        *
+ **************************************************************************
+ *                                                                        *
+ *  This program is free software: you can redistribute it and/or modify  *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                   *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful,       *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *  GNU General Public License for more details.                          *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                        *
+ *************************************************************************/
+
+/*
+ Maximum data rate of CY7C67300 is 8MHz.
+ */
+
+module hpi_controller
+  (clk,
+   reset,
+   /* CY7C67300 wiring */
+   hpi_address,
+   hpi_data,
+   hpi_oen,
+   hpi_wen,
+   hpi_csn,
+   hpi_irq,
+   hpi_resetn,
+   /* Control */
+   splat
+   /* ... */
+   );
+
+   /* There are exactly four HPI addresses: */
+   localparam [1:0]
+     HPI_REG_DATA = 2'b00,     /* R/W */
+     HPI_REG_MAILBOX = 2'b01,  /* R/W */
+     HPI_REG_ADDRESS = 2'b10,  /* W */
+     HPI_REG_STATUS = 2'b11;   /* R */
+
+   /* States for FSM */
+   localparam [1:0]
+     STATE_IDLE = 0,
+     STATE_SET_ADDR_REG, = 1,
+     STATE_ADDRESS_WRITE = 2,
+     STATE_DATA_WRITE = 3;
+   
+     
+
+     
+   /****************************************/
+   input 	     splat; /* For testing */
+   /****************************************/
+   
+   input clk; /* 32MHz (4x max HPI freq.) */
+   input reset; /* Active-high */
+   
+   output wire [1:0] hpi_address;
+   inout wire [15:0] hpi_data;
+   output wire 	     hpi_oen;
+   output wire 	     hpi_wen;
+   output wire 	     hpi_csn;
+   input wire 	     hpi_irq;
+   output wire 	     hpi_resetn;
+
+   assign hpi_csn = 0; /* For now, pretend ACE doesn't exist */
+   
+   reg [1:0] 	     hpi_ctl_addr_reg;
+   assign hpi_address[1:0] = hpi_ctl_addr_reg;
+
+   reg 		     hpi_ctl_dir; /* HPI direction; 0: read, 1: write. */
+   
+   reg [15:0] 	     hpi_data_out;
+   assign hpi_data = hpi_ctl_dir ? hpi_data_out : 16'bz;
+
+   reg [31:0] 	     foo; /* For test */
+   
+   always @(posedge clk, posedge reset, posedge splat)
+     if (reset)
+       begin
+	  foo <= 32'b0;
+       end
+     else
+       begin
+	  if (splat)
+	    begin
+	       foo <= foo + 1;
+	    end
+       end
+
+   reg [23:0] tmr;
+   wire       one_hz;
+   assign one_hz = tmr[23];
+
+   wire       eight_mhz; /* Max data rate */
+   assign eight_mhz = tmr[2];
+
+   reg [1:0] st; /* FSM */
+
+   wire      idling;
+   assign idling = (st == STATE_IDLE);
+   
+   /* write if not reading */
+   assign hpi_wen = (~hpi_ctl_dir) && eight_mhz && (~idling);
+
+   /* read is already active-low */
+   assign hpi_oen = hpi_ctl_dir && eight_mhz && (~idling);
+
+   
+   always @(posedge clk, posedge reset)
+     if (reset)
+       begin
+	  tmr <= 0;
+       end
+     else
+       begin
+	  tmr <= tmr + 1;
+       end
+   
+   always @(posedge clk, posedge reset)
+     if (reset)
+       begin
+	  hpi_ctl_dir <= 0; /* Tristate the HPI bus on reset */
+	  hpi_ctl_addr_reg <= HPI_REG_STATUS;
+	  st = STATE_IDLE;
+       end
+     else
+       begin
+	  case (st)
+	    STATE_IDLE:
+	      begin
+		 hpi_ctl_dir <= 0;
+		 st = one_hz ? STATE_SET_ADDR_REG ? STATE_IDLE;
+	      end
+	    STATE_SET_ADDR_REG:
+	      begin
+		 hpi_ctl_addr_reg <= HPI_REG_ADDRESS;
+		 hpi_ctl_dir <= 1; /* Write */
+		 st = STATE_ADDRESS_WRITE;
+	      end
+	    STATE_ADDRESS_WRITE:
+	      begin
+		 
+		 st = STATE_DATA_WRITE;
+	      end
+	    STATE_DATA_WRITE:
+	      begin
+		 st = STATE_IDLE;
+	      end
+       end
+
+endmodule
--- ml501/stierlitz_demo_top.v
+++ ml501/stierlitz_demo_top.v
@@ -18,22 +18,83 @@
  *                                                                        *
  *************************************************************************/
 
-
 `timescale 1ns/1ps
 
-module stierlitz_demo_top (sys_clk, usb_hpi_reset_n, CBUTTON);
+`include "hpi_controller.v"
 
-   output wire RLED;
-   output wire TLED;
-   output wire GLED;
-   input wire 	CBUTTON;
-   output wire 	usb_hpi_reset_n;
 
-   wire 	usbreset = CBUTTON;
-   assign GLED = usbreset;
+module stierlitz_demo_top
+  (sys_clk,          /* 100MHz main clock line */
+   sys_rst_pin,      /* Master reset */
+   /************* Cypress CY7C67300 *************/
+   sace_usb_a,       /* CY HPI Address bus (two bits) */
+   sace_usb_d,       /* CY HPI Data bus */
+   sace_usb_oen,     /* CY HPI nRD */
+   sace_usb_wen,     /* CY HPI nWR */
+   usb_csn,          /* CY HPI nCS */
+   usb_hpi_reset_n,  /* CY HPI nRESET */
+   usb_hpi_int,      /* CY HPI INT */
+   /*********************************************/
+   sace_mpce,        /* Xilinx ACE nCS */
+   /*********************************************/
+   CBUTTON,          /* Center Button */
+   EBUTTON,          /* East Button */
+   /*********************************************/
+   led_byte          /* LED bank, 8 bits wide */
+   );
 
-   assign usb_hpi_reset_n = ~usbreset;
-  
    
-endmodule
+   /* The basics */
+   input wire sys_clk;
+   input wire sys_rst_pin;
+   input wire CBUTTON;      /* These buttons are active-high */
+   input wire EBUTTON;
+   output wire [7:0] led_byte;
+
+   /* Xilinx ACE - must be disabled for Cypress CY to work */
+   output wire 	sace_mpce;
+   
+   /* CY7C67300 */
+   output wire 	sace_usb_oen;
+   output wire 	sace_usb_wen;
+   output wire 	usb_csn;
+   input wire  	usb_hpi_int;
+   output wire [1:0] sace_usb_a;
+   inout wire [15:0] sace_usb_d;
+   output wire 	usb_hpi_reset_n;
+   
+   /* CY manual reset */
+   wire 	usbreset = CBUTTON | ~sys_rst_pin; /* tie rst to main rst */
+   assign usb_hpi_reset_n = ~usbreset;
+
+   /* 32 MHz (x4) clock for HPI interface */
+   wire 	hpi_clock;
+   DCM hpi_clock_dcm (.CLKIN(sys_clk), .CLKFX(hpi_clock));
+   defparam hpi_clock_dcm.CLKFX_MULTIPLY = 8;
+   defparam hpi_clock_dcm.CLKFX_DIVIDE = 25;
+
+   assign sace_mpce = 1; /* Switch off ACE to free the bus it shares with CY */
+
+   wire 	hpi_manual_test = EBUTTON; /* temporary manual toggle to run tester */
+
+   wire 	usb_irq = usb_hpi_int; /* HPI IRQ is active-high */
+   assign led_byte[0] = ~usb_irq; /* LEDs are active-high */
+   assign led_byte[7:1] = 7'b0;
+   
+   hpi_controller stierlitz(.clk(hpi_clock),
+			    .reset(usbreset),
+			    .hpi_resetn(usb_hpi_reset_n),
+			    .hpi_csn(usb_csn),
+			    .hpi_oen(sace_usb_oen),
+			    .hpi_wen(sace_usb_wen),
+			    .hpi_irq(usb_hpi_int),
+			    .hpi_address(sace_usb_a),
+			    .hpi_data(sace_usb_d),
+			    .splat(hpi_manual_test)
+			    );
+   
+			    
+   
+   
+ndmodule
 
